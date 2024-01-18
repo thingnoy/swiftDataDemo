9ad1d1c393d9be4a0775e0ef764bedf7d2d00986
supasai(boy) <boyskypart@gmail.com>
2024-01-18 09:53:40 +0800

    Initial Commit

---
+++ Gusto/GustoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustoApp.swift
+//  Gusto
+//
+//  Created by supasai haplakhon on 18/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GustoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
